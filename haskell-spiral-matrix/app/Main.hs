324f15be2aeea9b43f6132238af6985153e37a4e
hsh814 <hsh814@naver.com>
2020-02-20 22:53:05 +0900

    haskell-spiral-matrix: In process

--- haskell-spiral-matrix/app/Main.hs
+++ haskell-spiral-matrix/app/Main.hs
@@ -1,6 +1,51 @@
-module Main where
+import System.Environment
+import Data.List
 
-import Lib
 
-main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    let 
+        n = show $ head args
+        matrix = spiral n n
+    mapM_ printList $ foldr (:) [] $ filterRow matrix
+
+data Direction = Right' | Down | Left' | Up
+    deriving (Show, Eq)
+
+spiral :: Int -> Int -> [(Int, Int, Int, Direction)]
+spiral n 1 = [(1, 1, 1, Right')]
+spiral n m = 
+    let
+        (_, col, row, direction) = head $ spiral n (m-1) 
+    in 
+        case direction of
+            Right' -> if col == (n-1) 
+                then (m, col + 1, row, Down) : spiral n (m-1)
+                else (m, col + 1, row, Right') : spiral n (m-1)
+            Down -> if row == (n-1)
+                then (m, col, row + 1, Left') : spiral n (m-1)
+                else (m, col, row+1, Down) : spiral n (m-1)
+            Left' -> if col == 2
+                then (m, col - 1, row, Up) : spiral n (m-1)
+                else (m, col - 1, row, Left') : spiral n (m-1)
+            Up -> if row == 2
+                then (m, col, row - 1, Right') : spiral n (m-1)
+                else (m, col, row - 1, Up) : spiral n (m-1)
+
+printList :: (Show a) => [a] -> IO ()
+printList [] = putStr "\n"
+printList (x:xs) = do
+    putStr $ show x ++ " "
+    printList xs
+
+filterRow :: Int -> [(Int, Int, Int, Direction)] -> [Int]
+filterRow row matrix = 
+    let
+        row1 = filter (\(num, col, row2, direction) -> row == row2) matrix 
+        row2 = sortOn (\(_, col, _, _) -> col) row1
+    in 
+        onlyNums row2
+
+onlyNums :: [(Int, Int, Int, Direction)] -> [Int]
+onlyNums [] = []
+onlyNums ((num, _, _, _) : ls) = num : onlyNums ls 
